9e2fb1a2faf6b8565de26be5f385b81f858a0f7e
Namankumar608x <nmukati3000@gmail.com>
2025-04-06 18:44:38 +0530

    sports connect to article

--- sports/sports.js
+++ sports/sports.js
@@ -29,6 +29,8 @@ function displaySportsNews(newsArray, containerId) {
                         <p class="card-text">${news.content.substring(0, 150)}...</p>
                         <p class="text-muted"><strong>Published on:</strong> ${new Date(news.date_published).toLocaleDateString()}</p>
                         <p class="text-muted"><strong>Tags:</strong> ${news.tags.join(", ")}</p>
+                        <a href="../article.html?category=Sports&id=${news._id}" class="btn btn-primary">Read More</a>
+
                     </div>
                 </div>
             </div>
